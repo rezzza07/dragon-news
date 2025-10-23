9b2089ad5fd21c3f1960a70741f7563b9c2932da
rezzza07 <notfunnyatall0007@gmail.com>
2025-10-23 13:17:28 +0600

    added register and login

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { NavLink } from 'react-router';
+import { Link, NavLink } from 'react-router';
 import user from '../assets/user.png'
 
 const Navbar = () => {
     return (
         <div className='flex justify-between items-center'>
-            <div></div>
+            <div><Link></Link></div>
             <div className='nav flex gap-5 text-accent'>
                 <NavLink to="/">Home</NavLink>
                 <NavLink to="/about">About</NavLink>
@@ -13,7 +13,7 @@ const Navbar = () => {
             </div>
             <div className='login-btn flex gap-5'>
                 <img src={user} alt="" />
-                <button className='btn btn-primary px-10'>Login</button>
+                <Link to="/auth/login" className='btn btn-primary px-10'> Login </Link> 
             </div>
         </div>
     );
